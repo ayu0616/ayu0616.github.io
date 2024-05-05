77b2ba859dc969bc4c4ed7c59d5d4e65f6450c8e
ayu0616 <ayumuogawa0616@gmail.com>
2024-05-05 21:45:08 +0900

    コンポーネントをいじった

--- src/components/Markdown/Markdown.tsx
+++ src/components/Markdown/Markdown.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import ReactMarkdown from 'react-markdown'
 import remarkBreaks from 'remark-breaks'
 import remarkGfm from 'remark-gfm'
@@ -11,6 +12,22 @@ const Markdown: React.FC<MarkdownProps> = ({ children = '' }) => {
         <div className='grid gap-4'>
             <ReactMarkdown
                 components={{
+                    a: ({ children, href }) => (
+                        <Link
+                            className='block text-emerald-800 underline decoration-1 underline-offset-1'
+                            href={href ?? '#'}
+                            rel={
+                                href?.startsWith('http')
+                                    ? 'noopener noreferrer'
+                                    : undefined
+                            }
+                            target={
+                                href?.startsWith('http') ? '_blank' : undefined
+                            }
+                        >
+                            {children}
+                        </Link>
+                    ),
                     li: ({ children }) => (
                         <li className='flex'>
                             <div>{children}</div>
