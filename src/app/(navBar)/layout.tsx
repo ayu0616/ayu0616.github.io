2110ff4b8863b7d7fbad881a447dab25eb9dac38
ayu0616 <ayumuogawa0616@gmail.com>
2024-05-05 20:11:12 +0900

    スクロールを修正した

--- src/app/(navBar)/layout.tsx
+++ src/app/(navBar)/layout.tsx
@@ -32,7 +32,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                     ))}
                 </div>
             </nav>
-            <div className='flex-1'>{children}</div>
+            <div className='flex-1 overflow-y-auto'>{children}</div>
         </div>
     )
 }
