783ebfc6be5602126f8afad12faf7c77526337cf
ayu0616 <ayumuogawa0616@gmail.com>
2024-06-17 01:09:04 +0900

    目次の見た目を修正した

--- src/components/Markdown/components/Pre/TableOfContents.tsx
+++ src/components/Markdown/components/Pre/TableOfContents.tsx
@@ -28,11 +28,11 @@ const TableOfContents = ({
         return tocList.join('\n')
     }, [slug])
     return (
-        <div className='mx-auto w-fit space-y-4 rounded-md border border-emerald-800'>
+        <div className='mx-auto w-fit min-w-[50%] max-w-full rounded-md border border-emerald-800'>
             <div className='rounded-t-md bg-emerald-800 p-2 text-center text-lg text-white'>
                 Contents
             </div>
-            <Markdown className='p-4 pt-0' id='toc'>
+            <Markdown className='p-6' id='toc'>
                 {toc || '目次なし'}
             </Markdown>
         </div>
