671c829cdfb92f5e09f379e8657d6e1569fc05f8
ayu0616 <ayumuogawa0616@gmail.com>
2024-06-28 21:18:51 +0900

    レイアウトの修正

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -7,12 +7,12 @@ import NavLink from '@/components/Link/NavLink'
 import pages from '@/constant/pages'
 
 const NavBar = () => (
-    <nav className='sticky top-0 flex w-full items-center gap-6 bg-emerald-800 px-4 py-2 text-white'>
-        <Link href='/'>
+    <nav className='sticky top-0 flex h-20 w-full items-center gap-6 bg-emerald-800 px-4 py-2 text-white sm:h-24'>
+        <Link className='aspect-square h-full' href='/'>
             <Image
                 alt='icon'
                 blurDataURL={IconMini.src}
-                className='drag-none aspect-square rounded-full drop-shadow transition-transform duration-300 ease-in-out hover:scale-105'
+                className='drag-none aspect-square h-full rounded-full object-contain drop-shadow transition-transform duration-300 ease-in-out hover:scale-105'
                 height={64}
                 loading='lazy'
                 placeholder='blur'
@@ -25,11 +25,11 @@ const NavBar = () => (
                 <NavLink
                     key={path}
                     activeClassName='bg-white/10'
-                    className='flex h-full items-center gap-2 rounded-md px-2 transition-colors hover:bg-white/10'
+                    className='flex aspect-square h-full items-center justify-center gap-2 rounded-md px-2 text-xl transition-colors hover:bg-white/10 sm:aspect-auto sm:text-base'
                     href={path}
                 >
                     {icon}
-                    <span>{name}</span>
+                    <span className='hidden sm:block'>{name}</span>
                 </NavLink>
             ))}
         </div>
