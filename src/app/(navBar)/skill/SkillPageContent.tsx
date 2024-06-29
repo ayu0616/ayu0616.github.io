8d0f971133fc534779cae5e3470e1887d3feea26
ayu0616 <ayumuogawa0616@gmail.com>
2024-06-29 23:01:26 +0900

    グラフの表示を修正した

--- src/app/(navBar)/skill/SkillPageContent.tsx
+++ src/app/(navBar)/skill/SkillPageContent.tsx
@@ -10,7 +10,7 @@ const SkillPageContent = () => {
         'utf-8',
     )
     return (
-        <div className='mx-auto flex h-full w-dvw max-w-screen-lg flex-col gap-4 p-6'>
+        <div className='mx-auto flex h-[800px] w-dvw max-w-screen-lg flex-col gap-4 p-6'>
             <div className='space-y-2 rounded-md border bg-white p-4'>
                 <Markdown>{skillDescription}</Markdown>
             </div>
