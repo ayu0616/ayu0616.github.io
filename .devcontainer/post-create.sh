aa7d160575507f694d7c61c1a8365c663f025ff8
ayu0616 <ayumuogawa0616@gmail.com>
2024-09-12 12:33:31 +0000

    zshrcをgitに乗せる

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,9 @@
 #! /bin/sh
 
+# create symlink for .zshrc
+ln -sf /workspace/.devcontainer/.zshrc ~/.zshrc
+
+# Install zsh plugins
 if [ ! -d "${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-autosuggestions" ]; then
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-autosuggestions
 fi
@@ -8,5 +12,6 @@ if [ ! -d "${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-syntax-highlig
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-syntax-highlighting
 fi
 
+# Install node_modules
 pnpm i
 pnpm codegen
