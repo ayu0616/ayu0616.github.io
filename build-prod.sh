4d5746241284752032b57053cd4c2c28f0173f69
ayu0616 <ayumuogawa0616@gmail.com>
2024-11-07 17:36:05 +0900

    blog-contents.zipの存在を確認し、削除する処理を追加

--- build-prod.sh
+++ build-prod.sh
@@ -2,6 +2,9 @@
 
 cd $(dirname $0)
 
+if [ -f blog-contents.zip ]; then
+    rm blog-contents.zip
+fi
 zip -r blog-contents.zip blog-contents
 docker compose -f docker-compose.prod.yml --env-file .env.local build
 rm blog-contents.zip
