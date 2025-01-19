016122e15333bc5a3c6ae7c6072e8a7e28616ade
ayu0616 <ayumuogawa0616@gmail.com>
2025-01-19 22:02:24 +0900

    必要そうなファイルを追加

---
+++ main.ts
@@ -0,0 +1,17 @@
+// main.ts
+import { serve } from '@hono/node-server'
+import { serveStatic } from '@hono/node-server/serve-static'
+import handle from 'hono-react-router-adapter/node'
+// @ts-ignore
+import * as build from './build/server'
+import server from './server'
+
+server.use(
+    serveStatic({
+        root: './build/client',
+    }),
+)
+
+const handler = handle(build, server)
+
+serve({ fetch: handler.fetch, port: 3010 })
