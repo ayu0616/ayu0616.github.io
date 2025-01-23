3352912bcb06ac48b43fc9cf13741502a1c14364
ayu0616 <ayumuogawa0616@gmail.com>
2025-01-23 19:05:29 +0900

    プリレンダリングしようとしている
    このコミットの段階ではまだエラーが発生する
    パスエイリアスが効かない不具合？が存在するようなのでパスエイリアス設定を削除してからまた試してみる

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,26 @@
 import type { Config } from '@react-router/dev/config'
+import skillData from './app/constant/skillData'
+import { works } from './app/constant/works'
+// import { getBlogPageInfo } from 'server/api/blog'
 
 export default {
     // Config options...
     // Server-side render by default, to enable SPA mode set this to `false`
     ssr: true,
+
+    async prerender({ getStaticPaths }) {
+        const res = getStaticPaths()
+        return res
+            .concat(
+                skillData
+                    .filter((skill) => skill.id !== 'AtCoder')
+                    .map((skill) => `/skill/${skill.id}`),
+            )
+            .concat(Object.keys(works).map((slug) => `/work/${slug}`))
+        // .concat(
+        //     Object.keys(await getBlogPageInfo()).map(
+        //         (slug) => `/blog/${slug}`,
+        //     ),
+        // )
+    },
 } satisfies Config
