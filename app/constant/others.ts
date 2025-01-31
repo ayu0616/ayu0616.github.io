ca948e34b1810078dc23ea50928518a3d8dedb56
ayu0616 <ayumuogawa0616@gmail.com>
2025-01-31 10:41:38 +0900

    PRODの定義を変更

--- app/constant/others.ts
+++ app/constant/others.ts
@@ -1,6 +1,6 @@
 export const BASE_URL = 'https://www.hassaku0616.com'
 
 export const PROD =
-    import.meta.env.PROD || process.env.NODE_ENV === 'production'
+    process.env.NODE_ENV !== 'development' || import.meta.env.PROD
 
 export const GCS_URL = 'https://storage.googleapis.com/hassaku-blog-contents'
