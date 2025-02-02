f39fe6d667d939cfb3b141c3645feddfed4204ba
ayu0616 <ayumuogawa0616@gmail.com>
2025-02-02 16:27:31 +0900

    BlogPageCardコンポーネントのレイアウトを改善し、公開日を表示する機能を追加

--- app/components/BlogPageCard.tsx
+++ app/components/BlogPageCard.tsx
@@ -18,17 +18,34 @@ export const BlogPageCard: FC<BlogPageCardProps> = ({
     return (
         <Link key={slug} to={`/blog/${slug}`}>
             <div className="rounded-md bg-white p-6 shadow">
-                <h3 className="mb-2">{title}</h3>
-                <div className="space-y-1">
-                    <div className="flex flex-wrap gap-x-2 gap-y-1">
-                        {tags.length > 0 ? (
-                            tags.map((tag) => <BlogTag key={tag} tag={tag} />)
-                        ) : (
-                            <span className="text-gray-500">タグ無し</span>
-                        )}
+                <div className="flex gap-4">
+                    <div className="flex-1">
+                        <h3 className="mb-2">{title}</h3>
+                        <div className="space-y-1">
+                            <div className="flex flex-wrap gap-x-2 gap-y-1">
+                                {tags.length > 0 ? (
+                                    tags.map((tag) => (
+                                        <BlogTag key={tag} tag={tag} />
+                                    ))
+                                ) : (
+                                    <span className="text-gray-500">
+                                        タグ無し
+                                    </span>
+                                )}
+                            </div>
+                            <div>
+                                公開日：{' '}
+                                {dayjs(publishedAt).format('YYYY-MM-DD')}
+                            </div>
+                        </div>
                     </div>
                     <div>
-                        公開日： {dayjs(publishedAt).format('YYYY-MM-DD')}
+                        <img
+                            alt="thumbnail"
+                            className="h-32 w-32 rounded-md object-cover"
+                            loading="lazy"
+                            src={'https://placehold.jp/600x600.png'} // TODO: 画像を取得する
+                        />
                     </div>
                 </div>
             </div>
